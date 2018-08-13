854ab93d102d533b4412ea38e42a9db88fc8674a
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-08-13 00:31:32 -0400

    that last invalid when non-unique was a tricky one

---
+++ spec/factories/author_factory.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :author do
+    name "ben"
+    phone_number 4084649007
+  end
+end
---
+++ spec/factories/post_factory.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :post do
+    title "You Won't Believe How SJS Will Win The Stanley Cup In 2019"
+    content "The San Jose Sharks are super duper awesome. The San Jose Sharks are super duper awesome. The San Jose Sharks are super duper awesome. The San Jose Sharks are super duper awesome. The San Jose Sharks are super duper awesome. The San Jose Sharks are super duper awesome."
+    summary "A post about the san jose sharks"
+    category "Non-Fiction"
+  end
+end
